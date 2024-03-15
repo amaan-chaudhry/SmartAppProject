bdea05e5fe77ca84834e255847d9949f8e7f2271
amaan-chaudhry <162828343+amaan-chaudhry@users.noreply.github.com>
2024-03-15 21:39:55 +0000

    Update wsgi.py

--- MySite/wsgi.py
+++ MySite/wsgi.py
@@ -1,18 +1,8 @@
-"""
-WSGI config for MySite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-setting_module =  'MySite.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'MySite.settings'
+settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", setting_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
