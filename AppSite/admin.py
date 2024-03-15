424eb8e4e4d99c8e38c4ce927da0a2f93452ccc2
amaan-chaudhry <162828343+amaan-chaudhry@users.noreply.github.com>
2024-03-15 21:35:19 +0000

    Update admin.py

--- AppSite/admin.py
+++ AppSite/admin.py
@@ -1,26 +1,4 @@
 from django.contrib import admin
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from .models import User
+from .models import Project
 
-
-class MyUserCreationForm(UserCreationForm):
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-        try:
-            User._default_manager.get(username=username)
-        except User.DoesNotExist:
-            return username
-        raise forms.ValidationError(self.error_messages['duplicate_username'])
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-
-
-class UserAdmin(AuthUserAdmin):
-    add_form = MyUserCreationForm
-    update_form_class = UserChangeForm
-
-
-admin.site.register(User, UserAdmin)
+admin.site.register(Project)
