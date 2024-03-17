01b42e6f69b3a5131759a5e7e3c445c0a1b40061
amaan-chaudhry <162828343+amaan-chaudhry@users.noreply.github.com>
2024-03-17 13:24:14 +0000

    Update deployment.py

--- MySite/deployment.py
+++ MySite/deployment.py
@@ -21,7 +21,7 @@ MIDDLEWARE = [
 ] 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = ( BASE_DIR.as_posix() + '/staticfiles' )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
