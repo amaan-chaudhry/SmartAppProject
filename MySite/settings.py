aecca9d0b4cb73c7ac683f20a76bd2465691aedb
amaan-chaudhry <162828343+amaan-chaudhry@users.noreply.github.com>
2024-03-17 00:57:54 +0000

    Add files via upload

--- MySite/settings.py
+++ MySite/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "AppSite",
+    "AppSite"
 ]
 
 MIDDLEWARE = [
@@ -123,7 +122,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('AppSite/static')),)
 STATIC_URL = '/AppSite/static/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
