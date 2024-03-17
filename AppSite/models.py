b0d002ce81f466e0a9fc01a34afd2ed3e6fc1fd0
amaan-chaudhry <162828343+amaan-chaudhry@users.noreply.github.com>
2024-03-17 16:20:02 +0000

    Update models.py

--- AppSite/models.py
+++ AppSite/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-class User(AbstractUser):
-    subscribe_newsletters = models.BooleanField(default=True)
-    old_id = models.IntegerField(null=True, blank=True)
-    old_source = models.CharField(max_length=25, null=True, blank=True)
+
+class Project(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    description = models.CharField(max_length=500,blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name 
