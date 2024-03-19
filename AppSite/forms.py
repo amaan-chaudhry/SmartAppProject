52e653f1849d1497194ddda5ce0cf1bc85dfbeba
amaan-chaudhry <162828343+amaan-chaudhry@users.noreply.github.com>
2024-03-19 03:31:43 +0000

    Update forms.py

--- AppSite/forms.py
+++ AppSite/forms.py
@@ -8,5 +8,5 @@ class ProjectForm(forms.ModelForm):
     name =  forms.CharField(label="Name",required=True)
     description = forms.CharField(label="Description", required=False,help_text="", widget=forms.Textarea(attrs={'rows': 10, 'cols': 85}))
     class Meta: 
-        model = Project
+        model = Post
         fields = ['name', 'description']  
