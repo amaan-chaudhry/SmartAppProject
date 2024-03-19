df090b25a2642e2eaf9f836b00febd7ac11b887c
amaan-chaudhry <162828343+amaan-chaudhry@users.noreply.github.com>
2024-03-19 03:16:50 +0000

    Update models.py

--- AppSite/models.py
+++ AppSite/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
-class Project(models.Model):
-    name = models.CharField(max_length=500, unique=True)
-    description = models.CharField(max_length=500,blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
-    edited_at = models.DateTimeField(auto_now=True)
-
+from django.contrib.auth.models import User
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content= models.TextField()
     def __str__(self):
-        return self.name 
+        return self.title
