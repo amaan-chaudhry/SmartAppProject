c2b4a6b834252048fc66e3c266f510a0e078e716
Knowledge00 <121947628+Knowledge00@users.noreply.github.com>
2024-03-28 01:31:01 +0000

    Update deployment.py

--- MySite/deployment.py
+++ MySite/deployment.py
@@ -24,18 +24,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-'''conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "Amaan",
-        'HOST': "smartappazure.postgres.database.azure.com",
-        'USER': "Amaan",
-        'PASSWORD': "Goldenmile*!",
-    }
-}'''
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
